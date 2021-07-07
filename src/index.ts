71224522df24e1e80c2a506a2919149c799a5e30
kethan <ckethan1@gmail.com>
2021-07-07 11:29:03 +0530

    remove handlers and routes in unlisten

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { decode, encode, pathToRegex } from './utils';
-// import { Req, Res, ErrorHandler, MatchMiddleware, Middleware, NextHandler, NoMatchMiddleware, Promisable } from "./types";
 import Browter from './browter';
 export { decode, encode, pathToRegex, Browter };
